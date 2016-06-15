99f54a82a84d77f7dd03777c8bfc65ab1353a20a
Shahadat Hossain <shahadat.cseru@gmail.com>
2016-06-15 23:31:55 +0000

    install http,cors,body-parser | working as rest api

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-//app.js
-
 'use strict';
 
 var express = require('express');
-console.log("It works...");
+var http = require('http');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+//application
+var app = express();
+
+var server = http.createServer(app);
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+
+//routes
+app.get('/', function(req, res) {
+	res.send("Hello todo app.");
+});
+
+app.get('/todo', function(req, res) {
+	console.log("get request from ", req.path);
+	res.send(["Hello todo app."]);
+});
+
+//listen
+server.listen(3001, function(){
+	console.log("Todo app, server started at 3001.");
+});
